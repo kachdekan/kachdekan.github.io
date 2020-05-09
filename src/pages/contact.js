24c317030425adc5d7b775e9bfc219bd5feedaf0
Rambo Engr <kachdekan@gmail.com>
2020-05-09 13:47:21 +0200

    My initial Commit

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Leave me a message</h1>
+            <p>For small gigs:</p>
+            <a href = "mailto: kachdekan@gmail.com">Send Email</a>
+        </div>
+    )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>The Rambo Engineer Blog</h1>
+            <p>My name is Kachi and am I am Learning Gatsby as I develop my Blog</p>
+
+            <p>Need an Engineer? <Link to="/contact">Contact Me</Link></p>
+        </div>
+    )
+}
+
+export default IndexPage
